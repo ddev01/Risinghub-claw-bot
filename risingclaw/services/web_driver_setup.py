2e53e38b84e8026dc6c908aba84e772b5852178e
ddev01 <71857399+ddev01@users.noreply.github.com>
2024-05-31 21:15:44 +0200

    switch to firefox & geckodriver for ARM64

--- risingclaw/services/web_driver_setup.py
+++ risingclaw/services/web_driver_setup.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 from ..utilities.logger import time_print
 
 
@@ -9,19 +9,12 @@ class WebDriverSetup:
         time_print("Initializing WebDriverSetup")
         self.driver = self.setup_driver()
 
-    def setup_driver(self) -> webdriver.Chrome:
-        time_print("Setting up Chrome WebDriver")
+    def setup_driver(self) -> webdriver.Firefox:
+        time_print("Setting up Firefox WebDriver")
         options = Options()
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
-        # options.add_argument("--disable-images")
-        options.add_experimental_option(
-            "prefs",
-            {
-                # "profile.default_content_setting_values.images": 2,
-                # "profile.managed_default_content_settings.images": 2,
-                "profile.default_content_settings.cookies": 2,
-            },
-        )
-        service = Service(executable_path="/opt/homebrew/bin/chromedriver")
-        return webdriver.Chrome(service=service, options=options)
+        options.add_argument("--headless")
+        options.binary_location = '/opt/firefox-128.0a1/firefox'
+        service = Service(executable_path='/usr/local/bin/geckodriver')
+        return webdriver.Firefox(service=service, options=options)
